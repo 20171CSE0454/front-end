3e664bfef96c23f4ce652da08767d43644b32819
20171CSE0454 <60249651+20171CSE0454@users.noreply.github.com>
2021-04-07 16:08:03 +0530

    frontend_

---
+++ src/App.js
@@ -0,0 +1,254 @@
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route,  NavLink } from "react-router-dom";
+import "./App.css";
+import AddPayment from "./components/AddPayment";
+import UpdatePayment from "./components/UpdatePayment";
+import ViewPayment from "./components/ViewPayment";
+import AddUser from './components/AddUser';
+import UpdateUser from './components/UpdateUser';
+import ViewUser from './components/ViewUser';
+import AddLogin from './components/AddLogin';
+import ViewLogin from './components/ViewLogin';
+import ChangePassword from './components/ChangePassword';
+import ViewCollege from "./components/ViewCollege";
+import AddCollege from "./components/AddCollege";
+import UpdateCollege from "./components/UpdateCollege";
+import AddCourse from './components/AddCourse';
+import UpdateCourse from './components/UpdateCourse';
+import ViewCourse from './components/ViewCourse';
+import AddBranch from './components/AddBranch';
+import ViewBranch from './components/ViewBranch';
+import UpdateBranch from './components/UpdateBranch';
+import AddProgram from './components/AddProgram';
+import ViewProgram from './components/ViewProgram';
+import UpdateProgram from './components/UpdateProgram';
+import AddProgramScheduled from './components/AddProgramScheduled';
+import UpdateProgramScheduled from './components/UpdateProgramScheduled';
+import ViewProgramScheduled from './components/ViewProgramScheduled';
+
+function App() {
+  return (
+    <Router>
+      <div  id="bimg" className="container-fluid "  style={{
+            backgroundImage:`url("https://presidencyuniversity.in/content/uploads/2017/07/Infra_04.jpg")` ,
+            backgroundSize: "cover",
+            height:"100vh"}} >
+      <div className="App">
+        <>
+          <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Online Admission System</Navbar.Brand>
+            <ul className="navbar-nav float-right">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse
+                id="responsive-navbar-nav"
+                variant="dark"
+              ></Navbar.Collapse>
+
+
+              
+            <NavDropdown title="Login" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/viewlogin">
+                    View Login
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addlogin" variant="dark">
+                    Add Login
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>              
+            </NavDropdown>
+            </ul>
+            <NavDropdown title="User" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+               <ul>
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  
+                  <NavLink className="nav-link active" aria-current="page" to="/viewuser">
+                    View User
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/adduser" variant="dark">
+                    Add User
+                  </NavLink>
+                </NavDropdown.Item>
+                </ul>
+              </Nav>  
+            </NavDropdown>
+            
+
+             <NavDropdown title="Payment" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/viewpayment">
+                    View Payment
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addpayment" variant="dark">
+                    Add Payment
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>
+            </NavDropdown>
+
+            <NavDropdown title="College" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/viewcollege">
+                    View College
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addcollege" variant="dark">
+                    Add College
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>              
+            </NavDropdown>
+
+            <NavDropdown title="Course" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/viewcourse">
+                    View Course
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addcourse" variant="dark">
+                    Add Course
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>              
+            </NavDropdown>
+
+            <NavDropdown title="Branch" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/branches">
+                    View Branch
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addbranch" variant="dark">
+                    Add Branch
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>              
+            </NavDropdown>
+
+            <NavDropdown title="Program" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/programs">
+                    View Program
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addprogram" variant="dark">
+                    Add Program
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>              
+            </NavDropdown>
+
+            <NavDropdown title="Program Schedule" id="collasible-nav-dropdown">
+              <Nav className="mr-auto">
+                <NavDropdown.Item href="#action/3.1" className="bg-secondary">
+                  {" "}
+                  <NavLink className="nav-link active" aria-current="page" to="/programScheduled">
+                    View ProgramSchedule
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="bg-secondary">
+                  <NavLink className="nav-link" to="/addprogramScheduled" variant="dark">
+                    Add ProgramSchedule
+                  </NavLink>
+                </NavDropdown.Item>
+              </Nav>              
+            </NavDropdown>
+          </Navbar>
+          <br />
+        </>
+        <Switch>
+          <Route exact path="/viewpayment">
+            <ViewPayment />
+          </Route>
+
+          <Route path="/addpayment">
+            <AddPayment />
+          </Route>
+          <Route path="/updatepayment/:paymentId" component={UpdatePayment}></Route>
+
+           <Route exact path="/viewuser">
+            <ViewUser />
+          </Route>
+          <Route path="/adduser">
+            <AddUser />
+          </Route>
+          <Route path="/updateuser/:userId" component={UpdateUser}></Route>
+          <Route exact path="/addlogin">
+            <AddLogin />
+          </Route>
+          <Route path="/viewlogin">
+            <ViewLogin />
+          </Route>
+          <Route path="/change/:userId" component={ChangePassword}></Route>
+
+          <Route path="/addcollege">
+            <AddCollege />
+          </Route>
+
+          <Route path="/updatecollege/:collegeRegId" component={UpdateCollege}></Route>
+          <Route path="/viewcollege">
+            <ViewCollege />
+          </Route>
+
+          <Route exact path="/viewcourse">
+            <ViewCourse />
+          </Route>
+          <Route path="/addcourse">
+            <AddCourse />
+          </Route>
+          <Route path="/updateCourse/:courseId" component={UpdateCourse}></Route>
+
+          <Route exact path="/branches">
+            <ViewBranch />
+          </Route>
+          <Route path="/addbranch">
+            <AddBranch />
+          </Route>
+          <Route path="/updateBranch/:branchId" component={UpdateBranch}></Route>
+
+          <Route exact path="/programs">
+            <ViewProgram />
+          </Route>
+          <Route path="/addprogram">
+            <AddProgram />
+          </Route>
+          <Route path="/updateProgram/:programId" component={UpdateProgram}></Route>
+
+          <Route exact path="/programScheduled">
+            <ViewProgramScheduled />
+          </Route>
+          <Route path="/addprogramScheduled">
+            <AddProgramScheduled />
+          </Route>
+          <Route path="/updateProgramScheduled/:scheduleId" component={UpdateProgramScheduled}></Route>
+        </Switch>
+      </div>
+      </div>
+    </Router>
+  );
+}
+export default App;
---
+++ src/store/reducer.js
@@ -0,0 +1,178 @@
+const initialState = {
+    payments: [],
+    logins: [],
+    users: [],
+    colleges:[],
+    courses : [],
+    branches:[],
+    programs:[],
+    message: "",
+  };
+  
+  const reducer = (state = initialState, { type, payload }) => {
+    console.log(type);
+    switch (type) {
+      case "FIND_PAYMENTS":
+        return { ...state, payments: payload, message: "" };
+  
+      case "FIND_APPLICATION":
+        return { ...state, payments: payload, message: "" };
+  
+      case "FIND_PAYMENT":
+        return { ...state, payments: payload, updatePayment: payload.payment };
+  
+      case "ADD_PAYMENT":
+        return { ...state, payments: state.payments, message: payload.message };
+  
+      case "DELETE_PAYMENT":
+        var filteredList = state.payments.filter(
+          (payment) => payment.id !== payload.payment.id
+        );
+        return { ...state, payments: filteredList, message: "" };
+  
+      case "UPDATE_PAYMENT":
+        console.log(payload.message);
+        return { ...state, payments: state.payments, message: payload.message };
+  
+      case "FIND_USERS" :
+          return {users : payload,message:''}
+  
+      case "FIND_USER" :
+          return {users : payload,updateUser : payload.user}
+  
+      case "ADD_USER":
+          return {users: state.users,message:payload.message}
+  
+      case "DELETE_USER":
+          return { ...state, message: payload.message };
+  
+      case "UPDATE_USER":
+          return { ...state, message: payload.message, users: state.users };
+  
+        case "FIND_LOGINS" :
+          return {logins : payload,message:''}
+  
+      case "FIND_LOGIN" :
+              return {logins : payload,updateLogin : payload.login}
+  
+      case "ADD_LOGIN":
+          return {logins: state.users,message:payload.message}
+  
+  
+      case "DELETE_LOGIN":
+          return { ...state, message: payload.message };
+  
+      //  case "UPDATE_LOGIN":
+      //   return { ...state, message: payload.message };   
+  
+      case "CHANGE_PASSWORD":
+        return { ...state, message: payload.message};   
+                 
+              // case "FIND_LOGIN" :
+              // return {logins : payload,updateLogin : payload.login} 
+         
+                    
+                case "FIND_COLLEGE" :
+                    return {...state,colleges : payload,message:''}
+            
+                    case "ADD_COLLEGE":
+                        return {...state,colleges: state.colleges,message:payload.message}
+                
+                        case "UPDATE_COLLEGE":
+                            console.log(payload.message)
+                                return {...state,colleges: state.colleges,message:payload.message}
+            
+                case "DELETE_COLLEGE":
+                    var filteredList2 = state.colleges.filter((college)=> college.id !== payload.college.id)
+                    return {...state,colleges: filteredList2,message:''}
+                                
+                    case "FIND_COURSES" :
+                      return {courses : payload,message:''}
+              
+                  case "FIND_COURSE" :
+                          return {courses : payload,updateCourse : payload.course}
+              
+                  case "ADD_COURSE":
+                      return {courses: state.courses,message:payload.message}
+              
+                  case "DELETE_COURSE":
+                      console.log(payload)
+                      var filteredList1 = state.courses.filter((course)=> course.courseId !== payload.course.courseId)
+                      return {courses: filteredList1,message:''}
+                  // case "DELETE_COURSE":
+                  //     return { ...state, message: payload.message };
+              
+                  case "UPDATE_COURSE":
+                      console.log(payload.message)
+                          return {courses: state.courses,message:payload.message}
+              
+                  case "FIND_BRANCHES" :
+                      return {branches : payload,message:''}
+                  case "FIND_BRANCH" :
+                      return {branches : payload,updateBranch : payload.branch}
+                          
+                              // case "FIND_USER" :
+                              //         return {users : payload,updateUser : payload.user}
+                          
+                  case "ADD_BRANCH":
+                      return {branches : state.branches,message:payload.message}
+                          
+                  case "DELETE_BRANCH":
+                      filteredList = state.branches.filter((branch)=> branch.branchId !== payload.branch.branchId)
+                          return {branch: filteredList,message:''}
+                          
+                  case "UPDATE_BRANCH":
+                      console.log(payload.message)
+                          return {branches: state.branches,message:payload.message}
+              
+                  case "FIND_PROGRAMS" :
+                      return {programs : payload,message:''}
+              
+                  case "FIND_PROGRAM" :
+                      return {programs : payload,updateProgram : payload.program}
+                          
+                              // case "FIND_USER" :
+                              //         return {users : payload,updateUser : payload.user}
+                          
+                  case "ADD_PROGRAM":
+                      return {programs : state.programs,message:payload.message}
+                          
+                  case "DELETE_PROGRAM":
+                      filteredList = state.programs.filter((program)=> program.programId !== payload.program.programId)
+                          return {program: filteredList,message:''}
+                          
+                  case "UPDATE_PROGRAM":
+                      console.log(payload.message)
+                          return {programs: state.programs,message:payload.message}
+                  
+                          case "FIND_PROGRAMSCHEDULES" :
+                              return {programSchedules : payload,message:''}
+                      
+                          case "FIND_PROGRAMSCHEDULED" :
+                              return {programSchedules : payload,updateProgramScheduled : payload.programScheduled}
+                                  
+                                      // case "FIND_USER" :
+                                      //         return {users : payload,updateUser : payload.user}
+                                  
+                          case "ADD_PROGRAMSCHEDULED":
+                              return {programSchedules : state.programSchedules,message:payload.message}
+                                  
+                          case "DELETE_PROGRAMSCHEDULED":
+                              filteredList = state.programSchedules.filter((programScheduled)=> programScheduled.scheduleId !== payload.programScheduled.scheduleId)
+                                  return {programScheduled: filteredList,message:''}
+                                  
+                          case "UPDATE_PROGRAMSCHEDULED":
+                              console.log(payload.message)
+                                  return {programSchedules: state.programSchedules,message:payload.message}        
+                          
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
+  
+  
+  
+  
